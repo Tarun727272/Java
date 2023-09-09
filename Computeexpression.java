ed51aed00d67af063365566485b792c216ed62ac
Tarun Kumar Pandey <128689278+Tarun727272@users.noreply.github.com>
2023-09-09 10:12:55 +0530

    Create Computeexpression.java

---
+++ Computeexpression.java
@@ -0,0 +1,16 @@
+/*
+08- Write a Java program to compute the specified expressions and print the output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889*/
+
+public class Computeexpression {
+
+	public static void main(String[] args) {
+		System.out.println(
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+
+	}
+
+}
