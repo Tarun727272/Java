bdc39cef56385e57656b44f22f63540a9190acc3
Tarun Kumar Pandey <128689278+Tarun727272@users.noreply.github.com>
2023-10-05 13:02:02 +0530

    Create 13-Variable.java

---
+++ 13-Variable.java
@@ -0,0 +1,28 @@
+//Java Variables
+
+class Variables{
+	public static void main(String[] args){
+    //Create a String variable
+	String name="Tarun";
+	System.out.println(name);
+	
+	//Create a int variable
+    int age=21;
+	System.out.println(age);
+	
+	//Create a vlaue and assign later in java
+	int myNum;
+	myNum=25;
+	System.out.println(myNum);
+	
+	//Overwrite an existing variable value
+	int mynum=30;
+	mynum=25;
+	System.out.println(myNum);
+	
+	//Declare same variable in a single line
+	int x=15,y=20,z=25;
+	System.out.println(x+y+z);
+	
+	}
+}
